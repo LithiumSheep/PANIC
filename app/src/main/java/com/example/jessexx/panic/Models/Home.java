d08bbef00bd0598a3ad2543ec4444fa46189080d
Aki <xiaoyu.gao@capitalone.com>
2016-08-27 14:55:23 -0400

    updated vehicle and home models

--- app/src/main/java/com/example/jessexx/panic/Models/Home.java
+++ app/src/main/java/com/example/jessexx/panic/Models/Home.java
@@ -5,11 +5,20 @@ package com.example.jessexx.panic.Models;
  */
 public class Home extends Asset {
 
+    private String location;
+
     public Home(String name, String description, int PictureId, float value, CATEGORY category) {
         super(name, description, PictureId, value, category);
     }
 
     public Home(String name, int PictureId, float value, CATEGORY category) {
         super(name, PictureId, value, category);
+    }
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
     }
 }
--- app/src/main/java/com/example/jessexx/panic/Models/Vehicle.java
+++ app/src/main/java/com/example/jessexx/panic/Models/Vehicle.java
@@ -5,12 +5,48 @@ package com.example.jessexx.panic.Models;
  */
 public class Vehicle extends Asset {
 
-    public Vehicle(String name, String description, int PictureId, float value, CATEGORY category) {
+    public enum CONDITION{
+        NEW, USED;
+    }
+    private int year;
+
+    private String location;
+
+    private CONDITION condition;
+
+    public Vehicle(String name, String description, int PictureId, float value, CATEGORY category, int year,String location, CONDITION
+        condition) {
         super(name, description, PictureId, value, category);
+        this.year = year;
+        this.location = location;
+        this.condition = condition;
     }
 
     public Vehicle(String name, int PictureId, float value, CATEGORY category) {
         super(name, PictureId, value, category);
     }
 
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public CONDITION getCondition() {
+        return condition;
+    }
+
+    public void setCondition(CONDITION condition) {
+        this.condition = condition;
+    }
 }
