e8b77a4d16976f199fda9962bf5c4ebfe3b494b3
jessexx <jesse@sandboxx.us>
2016-08-27 13:22:59 -0400

    card item bare bones

--- app/src/main/java/com/example/jessexx/panic/Activities/UserAssetActivity.java
+++ app/src/main/java/com/example/jessexx/panic/Activities/UserAssetActivity.java
@@ -2,11 +2,12 @@ package com.example.jessexx.panic.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.jessexx.panic.R;
+import com.github.clans.fab.FloatingActionButton;
 import com.reimaginebanking.api.nessieandroidsdk.requestclients.NessieClient;
 
 public class UserAssetActivity extends AppCompatActivity {
@@ -17,31 +18,26 @@ public class UserAssetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_assets);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-
-
+        setupFAB();
         initialTasks();
+    }
+
+    private void setupFAB() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "fab tapped", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     private void initialTasks() {
         client = NessieClient.getInstance("api_key");
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
