9152634e4aa76595f8ea1e2925ca088fe89f4bc7
Emma Suzuki <emma.suzuki@capitalone.com>
2016-08-27 17:14:11 -0400

    Add vehicle design

--- app/src/main/java/com/example/jessexx/panic/Adapters/AssetAdapter.java
+++ app/src/main/java/com/example/jessexx/panic/Adapters/AssetAdapter.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.jessexx.panic.Activities.AssetDetailActivity;
 import com.example.jessexx.panic.Models.Asset;
