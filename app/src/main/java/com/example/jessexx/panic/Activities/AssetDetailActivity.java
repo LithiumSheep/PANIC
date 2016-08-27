e3707202ed6fb8801eed776b09e9212cea2f45a8
jessexx <jesse@sandboxx.us>
2016-08-27 11:44:42 -0400

    compile error

--- app/src/main/java/com/example/jessexx/panic/Activities/AssetDetailActivity.java
+++ app/src/main/java/com/example/jessexx/panic/Activities/AssetDetailActivity.java
@@ -12,7 +12,7 @@ public class AssetDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asset_detail);
-g
+
 
     }
 }
